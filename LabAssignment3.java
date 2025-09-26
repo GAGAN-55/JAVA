c2ce9fa8eb971a370858ae5a6191024ed1a7581f
GAGAN-55 <2401730137@krmu.edu.in>
2025-09-26 20:28:18 +0530

    Added successfully

---
+++ LabAssignment3.java
@@ -0,0 +1,58 @@
+//Base class Shape
+
+class Shape {
+    //Constructor
+    public Shape() {
+        // Base class constructor 
+    }
+
+    public void area(){
+        System.out.println("Area not defined for this Shape. ");
+    }
+}
+
+//Subclass Circle
+class Circle extends Shape {
+    private double radius;
+
+    //Constructor to initialize the radius of the circle
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    //Overriding the area method to calculate area of a circle 
+    public void area() { 
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}
+
+//Subclass Rectangle
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    //Constructor to initialize the length and width of the rectangle
+    public Rectangle (double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    //Overriding the area method to calculate area of a circle 
+    public void area() { 
+        double area = length * width;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
+
+public class LabAssignment3 {
+    public static void main(String[] args) {
+        //Create an object of Circle
+        Circle circle = new Circle(4.0);
+        circle.area(); // Display area of circle
+
+        //Create an object of Rectangle
+        Rectangle rectangle = new Rectangle(5.9, 4.5);
+        rectangle.area(); // Display area of rectangle
+    }
+}
